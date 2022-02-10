efeba1e8f368bcba56907283af4dc923418193fc
Jamie <Sinclair>
2022-02-10 17:49:32 +0800

    url

--- gatsby-travel-website/gatsby-config.js
+++ gatsby-travel-website/gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Gatsby Travel App`,
+    description: `A simple site showcasing graphql gatsby images`,
+    author: `@jameleasin`,
+    siteUrl: `https://github.com/jameleasin/gatsby-travel-app.git`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
