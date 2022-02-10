e2d821d33c13eb6d78cd6bce2b1d86f9418cbd9a
Jamie <Sinclair>
2022-02-10 16:25:36 +0800

    herosection

--- gatsby-travel-website/src/pages/destinations.js
+++ gatsby-travel-website/src/pages/destinations.js
@@ -1,12 +1,13 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
+import HeroSection from "../components/HeroSection/index"
 import Seo from "../components/seo"
 
 const Destinations = () => (
   <Layout>
-    <Seo title="Page two" />
-    <h1>IDestinations</h1>
+    <Seo title="Destinations" />
+    <HeroSection />
     
   </Layout>
 )
